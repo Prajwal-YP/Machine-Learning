b1fd8c668280e30093438b172d149f7ffb60c7f3
Prajwal_Y_P <87578946+Prajwal-YP@users.noreply.github.com>
2025-02-22 00:23:32 +0530

    Update Linear_Regression_1.py

--- Week1/Regression Model/Linear_Regression_1.py
+++ Week1/Regression Model/Linear_Regression_1.py
@@ -4,68 +4,65 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+import matplotlib.gridspec as gridSpec
 
-# Univariate Linear Regression Model | Calculate the estimated price of house
-def UnivariateLinearRegressionModel(w,x,b):
-    return w*x+b
+class LinearRegression:
+    def __init__(self,w:float,b:float):
+        self.w=w
+        self.b=b
+    
+    def set_TrainingData(self,xTrain:np.array,yTrain:np.array):
+        self.x=xTrain
+        self.y=yTrain
+    
+    def predict(self,newinput:float):
+        output=self.w*newinput+self.b
+        return output
+    
+    def buildGraph(self,newIp:float,newOp:float):
+        gs=gridSpec.GridSpec(3,1,wspace=0.7,hspace=0.5)
 
-#Training data set
-xTrain=np.array([1.0,2.0])
-yTrain=np.array([300,500])
+        #Plot all the data points of training set
+        ax1=plt.subplot(gs[0,0])
+        ax1.set_xlabel='Size of the house'
+        ax1.set_ylabel='Price of the house'
+        ax1.scatter(self.x,self.y,c='red',marker='x')
+        ax1.set_title('Training set')
 
-gs = gridspec.GridSpec(4, 2)
+        #Plot all training data-set along with the linear function
+        ax2=plt.subplot(gs[1,0])
+        ax2.set_xlabel='Size of the house'
+        ax2.set_ylabel='Price of the house'
+        ax2.scatter(self.x,self.y,c='red',marker='o')
+        ax2.plot(self.x,self.y,c='blue')
+        ax2.set_title('Guessed Set/Model')
 
-#draw a Training set graph
-ax1=plt.subplot(gs[0,:2])
-ax1.scatter(x=xTrain,y=yTrain,marker='x',c='red')
-ax1.set_title('Training Set')
-ax1.set_xlabel('Size of the house')
-ax1.set_ylabel('price of the house')
-
-
-#Model parameters guess -1
-w=100
-b=100
-
-ax2=plt.subplot(gs[1,0])
-ax2.plot(xTrain,[UnivariateLinearRegressionModel(w,xTrain[0],b),UnivariateLinearRegressionModel(w,xTrain[1],b)])
-ax2.scatter(x=xTrain,y=yTrain,marker='x',c='red')
-ax2.set_title('Model parameters guess -1')
-ax2.set_xlabel('Size of the house')
-ax2.set_ylabel('price of the house')
-
-
-for i in range(len(xTrain)):
-    print(f'{xTrain[i]} sq.ft. size of house costs {UnivariateLinearRegressionModel(w,xTrain[i],b)}')
-
-#Model parameters guess -2
-w=200
-b=100
-
-ax3=plt.subplot(gs[1,1])
-ax3.plot(xTrain,[UnivariateLinearRegressionModel(w,xTrain[0],b),UnivariateLinearRegressionModel(w,xTrain[1],b)])
-ax3.scatter(x=xTrain,y=yTrain,marker='x',c='red')
-ax3.set_title('Model parameters guess -2')
-ax3.set_xlabel('Size of the house')
-ax3.set_ylabel('price of the house')
+        #PLot all training data-set, linear function and the newly predicted value
+        ax3=plt.subplot(gs[2,0])
+        ax3.set_xlabel='Size of the house'
+        ax3.set_ylabel='Price of the house'
+        ax3.scatter(self.x,self.y,c='red',marker='o')
+        ax3.plot(self.x,self.w*self.x+self.b,c='blue',label='linear function')
+        ax3.plot([0,newIp],[newOp,newOp],c='orange',marker='s',linestyle='--',label='predicted value')
+        ax3.plot([newIp,newIp],[0,newOp],c='orange',marker='s',linestyle='--')
+        ax3.set_title(f'Predicted (Size:{newIp}, Price:{newOp})')
+        
+        plt.legend()
+        plt.show()
 
 
-#Guess the proce for new house
+#Create linear regression with guess 1 parameters
+print('Enter guess-1 w and b parameters:')
+w=float(input('\tw:\t'))
+b=float(input('\tb:\t'))
+lr1=LinearRegression(w=w,b=b)
+lr1.set_TrainingData(xTrain=np.array([1.0,2.0]),yTrain=np.array([300,500]))
+lr1.buildGraph(1.3,lr1.predict(1.3))
 
-newSize=1.3
-newPrice=UnivariateLinearRegressionModel(w,newSize,b)
-
-ax4=plt.subplot(gs[2:4,:2])
-ax4.plot(xTrain,[UnivariateLinearRegressionModel(w,xTrain[0],b),UnivariateLinearRegressionModel(w,xTrain[1],b)],label='func')
-ax4.plot([0,newSize],[newPrice,newPrice],c='orange',linestyle='--',marker='o',label='predicted val')
-ax4.plot([newSize,newSize],[newPrice,0],c='orange',linestyle='--',marker='o')
-ax4.scatter(x=xTrain,y=yTrain,marker='x',c='red',label='TrainingSet')
-ax4.set_title('Working of Linear Regression')
-ax4.set_xlabel('Size of the house')
-ax4.set_ylabel('price of the house')
-ax4.legend()
-
-plt.box(1)
-plt.tight_layout()
-plt.show()
+#Create linear regression with guess 2 parameters
+print('Enter guess-2 w and b parameters:')
+w=float(input('\tw:\t'))
+b=float(input('\tb:\t'))
+lr2=LinearRegression(w=w,b=b)
+lr2.set_TrainingData(xTrain=np.array([1.0,2.0]),yTrain=np.array([300,500]))
+lr2.buildGraph(1.3,lr2.predict(1.3))
